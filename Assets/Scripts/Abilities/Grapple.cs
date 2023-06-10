8c0b57d251a363e2a43bd07ac0422fc742f97786
gringames <kerstin.pfaffinger@tum.de>
2023-06-10 20:56:38 +0200

    grapple start

---
+++ Assets/Scripts/Abilities/Grapple.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+namespace Abilities
+{
+    public class Grapple : MonoBehaviour
+    {
+        [SerializeField] private float speed = 6;
+        [SerializeField] private float maxRange = 7;
+
+        private GrappleAbility _grappleAbility;
+        private Vector3 _grapplePos;
+        private bool _fired;
+
+        private void Start()
+        {
+            _grappleAbility = GetComponentInParent<GrappleAbility>();
+        }
+        
+        private void Update()
+        {
+            if (!_fired) return;
+
+            transform.position += Vector3.right * (speed * Time.deltaTime);
+            
+            if (Vector3.Distance(transform.position, _grapplePos) > maxRange)
+            {
+                ResetGrapple();
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            Transform enemy = other.transform;
+            _grappleAbility.SetAttacked(enemy);
+            transform.parent = enemy;
+        }
+
+        public void Fire()
+        {
+            _fired = true;
+            transform.parent = null;
+            _grapplePos = transform.position;
+        }
+        
+        public void ResetGrapple()
+        {
+            _fired = false;
+            transform.parent = _grappleAbility.transform;
+            transform.position = transform.parent.position;
+        }
+    }
+}
--- Assets/Scripts/Abilities/GrappleAbility.cs
+++ Assets/Scripts/Abilities/GrappleAbility.cs
@@ -1,10 +1,46 @@
-﻿namespace Abilities
+﻿using UnityEngine;
+
+namespace Abilities
 {
     public class GrappleAbility : Ability
     {
+        private Transform _attackedEnemy;
+        private Grapple _grapple;
+        
+        private bool _attacked;
+
+
+        private void Start()
+        {
+            _grapple = GetComponentInChildren<Grapple>();
+        }
+
         protected override void DoAbility()
         {
-            // TODO: 
+            if (_attacked)
+            {
+                ResetAbility();
+                return;
+            }
+            
+            _grapple.Fire();
         }
+
+        public void SetAttacked(Transform attacked)
+        {
+            _attackedEnemy = attacked;
+            _attacked = true;
+        }
+
+        private void ResetAbility()
+        {
+            _attackedEnemy = null;
+            _attacked = false;
+            _grapple.ResetGrapple();
+        }
+
+
+
+
     }
 }
