5ea4e5fd01f16c1ab9d8a7336f7faf9b5e5b77ef
gringames <kerstin.pfaffinger@tum.de>
2023-06-10 23:09:30 +0200

    score data setup

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -1,11 +1,28 @@
-﻿using TMPro;
+﻿using System;
+using TMPro;
 using UnityEngine;
 
 public class GameData : MonoBehaviour
 {
-    [SerializeField] private TMP_Text tmpText;
+    [SerializeField] private TMP_Text gameScore;
+    [SerializeField] private TMP_Text outcome;
+    public int sessionHighScore;
+    public int currentScore;
 
-    private static int _score;
+    public static GameData Instance;
+    private int _score;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
 
     public void AddToScore(int gain)
     {
@@ -15,6 +32,27 @@ public class GameData : MonoBehaviour
 
     private void UpdateScoreText()
     {
-        tmpText.text = "" + _score;
+        gameScore.text = "" + _score;
+    }
+
+    public void DisplayOutcome()
+    {
+        outcome.enabled = true;
+        gameScore.enabled = false;
+
+        outcome.text = $"Current Score: {currentScore} \n High Score: {sessionHighScore}";
+    }
+
+    public void Reload()
+    {
+        currentScore = _score;
+        sessionHighScore = Math.Max(sessionHighScore, currentScore);
+    }
+
+    public void StartGame()
+    {
+        outcome.enabled = false;
+        gameScore.enabled = true;
+        _score = 0;
     }
 }
