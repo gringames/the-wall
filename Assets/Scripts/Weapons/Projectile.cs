1d934e2896c9cbd2670537eea25277b6f4d43fb9
pbrechtmann <44525951+pbrechtmann@users.noreply.github.com>
2023-06-10 13:12:23 +0200

    bouncy projectiles

--- Assets/Scripts/Weapons/Projectile.cs
+++ Assets/Scripts/Weapons/Projectile.cs
@@ -7,9 +7,23 @@ public class Projectile : MonoBehaviour
     [SerializeField] float _speed = 5f;
     [SerializeField] float _knockbackForce = 10f;
     [SerializeField] Rigidbody2D _rb;
+    [SerializeField] int _maxBounces = 1;
+
+    private int _bounces = 0;
 
     void Update()
     {
         _rb.velocity = transform.right * _speed;
+        _rb.angularVelocity = 0;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (_bounces >= _maxBounces)
+        {
+            Destroy(gameObject);
+        }
+        transform.right = Vector2.Reflect(transform.right, collision.GetContact(0).normal);
+        _bounces++;
     }
 }
