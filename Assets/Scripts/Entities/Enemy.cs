418bba253415c31eb2bffc76425313e06810d146
gringames <kerstin.pfaffinger@tum.de>
2023-06-10 18:13:02 +0200

    debug enemy shoot

--- Assets/Scripts/Entities/Enemy.cs
+++ Assets/Scripts/Entities/Enemy.cs
@@ -11,26 +11,19 @@ namespace Entities
         private void Start()
         {
             _pool = GetComponentInParent<EnemyPool>();
-            
-            string layerName = "PhysicsEnemy" + shape;
-            //gameObject.layer = LayerMask.NameToLayer(layerName);
-            
+
             InvokeRepeating(nameof(Shoot), _shotInterval, _shotInterval);
         }
 
         private void Update()
         {
             LookAtPos(player.position);
+
             if (IsDoneFalling)
             {
                 _pool.ResetEnemy(transform);
                 IsDoneFalling = false;
             }
-        }
-
-        public override void FallIntoVoid()
-        {
-            base.FallIntoVoid();
         }
 
         public void SetShotInterval(float interval)
